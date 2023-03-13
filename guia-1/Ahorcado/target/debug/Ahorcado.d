071875cacaeef5efa80de2ccee2e8d38884a9634
kevin untrojb <kuntrojb@192.168.1.5>
2023-03-13 20:27:34 -0300

    guia 1 ejercicio Ahoracado init

---
+++ guia-1/Ahorcado/target/debug/Ahorcado.d
@@ -0,0 +1,1 @@
+/Users/kuntrojb/Documents/fiuba/taller-1/guias/Rust-programs/guia-1/Ahorcado/target/debug/Ahorcado: /Users/kuntrojb/Documents/fiuba/taller-1/guias/Rust-programs/guia-1/Ahorcado/src/main.rs
---
+++ guia-1/Ahorcado/target/debug/deps/Ahorcado-6f585c41df6ba2bf.d
@@ -0,0 +1,5 @@
+/Users/kuntrojb/Documents/fiuba/taller-1/guias/Rust-programs/guia-1/Ahorcado/target/debug/deps/Ahorcado-6f585c41df6ba2bf: src/main.rs
+
+/Users/kuntrojb/Documents/fiuba/taller-1/guias/Rust-programs/guia-1/Ahorcado/target/debug/deps/Ahorcado-6f585c41df6ba2bf.d: src/main.rs
+
+src/main.rs:
